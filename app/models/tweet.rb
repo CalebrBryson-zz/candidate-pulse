744fa93d61c73ed1c873965ebf606b7dcd37f799
Caleb R Bryson <calebrbryson@gmail.com>
2015-11-28 16:17:36 -0700

    Running the Alchemi Api now in our app to get keyword analysis

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,17 @@
+require './alchemyapi'
+
+
 class Tweet < ActiveRecord::Base
   belongs_to :author
   has_many :hashtags
+
+  def sentiment
+    alchemyapi = AlchemyAPI.new()
+    keywords = alchemyapi.keywords("text", text)
+    keywords.each do |keyword|
+      puts keyword
+    end
+  end
+
+
 end
